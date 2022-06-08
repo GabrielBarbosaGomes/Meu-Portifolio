7b260b1cb0883fc97b80a4b6dd3e57fa636b588e
gabriel <gabryel122crf@gmail.com>
2022-06-08 00:39:32 -0300

    ajustando remoção das classes

--- js/js_menu.js
+++ js/js_menu.js
@@ -2,6 +2,7 @@ const menuzim = document.querySelector('#toggle');
 const main = document.querySelector('#main');
 const menu = document.querySelector('#menu')
 const menuItems = document.querySelector('#menu-items')
+const link= document.querySelector('.link-menu');
 
 menuzim.addEventListener('click', function(e) {
     menu.classList.toggle('menu-open');
@@ -10,5 +11,10 @@ menuzim.addEventListener('click', function(e) {
 
 main.addEventListener('click', function() {
     menu.classList.remove('menu-open');
-    menuItems.classList.remove('menu-items');
+    menuItems.classList.remove('menu-items-open');
 }) ;
+
+link.addEventListener('click', function(e){
+    menu.classList.remove('menu-open');
+    menuItems.classList.remove('menu-items-open');
+})
