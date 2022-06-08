5fb99a34413f2384dae6b4fa82da14d6fa157cd4
gabriel <gabryel122crf@gmail.com>
2022-06-08 02:55:22 -0300

    ajustando js menu

--- js/js_menu.js
+++ js/js_menu.js
@@ -3,18 +3,24 @@ const main = document.querySelector('#main');
 const menu = document.querySelector('#menu')
 const menuItems = document.querySelector('#menu-items')
 const link= document.querySelector('.link-menu');
+const back = document.querySelector('#check')
 
 menuzim.addEventListener('click', function(e) {
     menu.classList.toggle('menu-open');
     menuItems.classList.toggle('menu-items-open');
+    back.classList.toggle('checkbox-open');
 });
 
 main.addEventListener('click', function() {
     menu.classList.remove('menu-open');
     menuItems.classList.remove('menu-items-open');
+    back.classList.remove('checkbox-open');
+    $("#toggle").removeAttr('checked');
 }) ;
 
 link.addEventListener('click', function(e){
     menu.classList.remove('menu-open');
     menuItems.classList.remove('menu-items-open');
+    back.classList.remove('checkbox-open');
+    $("#toggle").removeAttr('checked');
 })
